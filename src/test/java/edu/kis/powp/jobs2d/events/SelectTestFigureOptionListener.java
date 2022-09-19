5591d9681ed4a315b2ae863a4b53188a11cb8450
tomasz <tomaszsztajkowski@gmail.com>
2022-09-19 14:46:52 +0200

    SelectTestFigureOptionListener

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import edu.kis.powp.jobs2d.drivers.DriverManager;
+import edu.kis.powp.jobs2d.factories.DrawFactory;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
@@ -22,6 +23,12 @@ public class SelectTestFigureOptionListener implements ActionListener {
 				break;
 			case "Figure Joe 2":
 				FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+				break;
+			case "rectangle":
+				DrawFactory.drawRectangle (-150,-150,150,200).execute(driverManager.getCurrentDriver());
+				break;
+			case "triangle":
+				DrawFactory.drawTriangle (-150,-150,150).execute(driverManager.getCurrentDriver());;
 				break;
 		}
 	}
